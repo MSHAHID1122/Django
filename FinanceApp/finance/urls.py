8da51824bede4f83e67d7adbc613213a954a57bb
MSHAHID1122 <samd13894@gmail.com>
2025-04-20 04:07:29 +0500

    implemented user authentication

--- FinanceApp/finance/urls.py
+++ FinanceApp/finance/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from finance.views import home,Home
+from finance.views import RegisterView
 urlpatterns = [
-    path('home/',home,name="home"),
-    path('myhome/',Home.as_view(),name="myhome")
+    # path('home/',home,name="home"),
+    # path('myhome/',Home.as_view(),name="myhome")
+
+    path("",RegisterView.as_view(),name="register view")
 ]
--- FinanceApp/finance/views.py
+++ FinanceApp/finance/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render,HttpResponse
 
 from django.views import View
 
-#function based view
-def home(request):
-    return HttpResponse("Hello World")
+# #function based view
+# def home(request):
+#     return HttpResponse("Hello World")
 
-#class based view
-class Home(View):
-    def get(self,request,*args,**kwargs):
-        return HttpResponse("<h1> Our class based component in Django</h1>")
+# #class based view
+# class Home(View):
+#     def get(self,request,*args,**kwargs):
+#         return HttpResponse("<h1> Our class based component in Django</h1>")
 
-class Home(View):
-    def get(self,request,*args,**kwargs):
-        return render(request,"finance/index.html")        
+# class Home(View):
+#     def get(self,request,*args,**kwargs):
+#         return render(request,"finance/index.html")        
+
+
+class RegisterView(View):
+    def get(self,request,*args,**kwars):
+        return render(request,"finance/register.html")
+
