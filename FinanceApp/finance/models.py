2b36943c3ae57a0603f6fd81fcb0d75e134a85d8
MSHAHID1122 <samd13894@gmail.com>
2025-04-30 00:50:53 +0500

    transcation model created and migrated usnig python manage.py migrate command

--- FinanceApp/finance/models.py
+++ FinanceApp/finance/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Transcation(models.Model):
     TRANSCATION_TYPES = [('Income','Income'),
                         ('Expense','Expense')]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=30,decimal_places=2)
     Transcation_type = models.CharField(max_length=20,choices= TRANSCATION_TYPES)
