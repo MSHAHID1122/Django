b7c165fb8465e8d85996316c11cf29b88618c9f8
MSHAHID1122 <samd13894@gmail.com>
2025-04-30 00:31:06 +0500

    implementing trasaction model

--- FinanceApp/finance/models.py
+++ FinanceApp/finance/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Transcation(models.Model):
+    TRANSCATION_TYPES = [('Income','Income'),
+                        ('Expense','Expense')]
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=30,decimal_places=2)
+    Transcation_type = models.CharField(max_length=20,choices= TRANSCATION_TYPES)
+    date = models.DateField()
+    category = models.CharField(max_length=255)
