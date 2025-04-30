a3b8a347715202f88b0cd9e503627532ee7f7b4b
MSHAHID1122 <samd13894@gmail.com>
2025-04-30 14:13:04 +0500

    working on transcation html page

--- FinanceApp/finance/forms.py
+++ FinanceApp/finance/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from django import forms
 from .models import Transcation
 
 
@@ -8,3 +9,9 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields=["username","email","password1","password2"]
+
+
+class TranscationForm(forms.ModelForm):
+    class Meta:
+        model = Transcation
+        fields = ['title','amount','Transcation_type','date','category']
