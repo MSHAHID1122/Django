79ba7e4dfcf0f36bc23f6ed2f87357cf144a9ff1
MSHAHID1122 <samd13894@gmail.com>
2025-05-02 22:02:49 +0500

    transcation list page

--- FinanceApp/finance/urls.py
+++ FinanceApp/finance/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from finance.views import RegisterView,Dashboard,TranscationView
+from finance.views import RegisterView,Dashboard,TranscationViewn,ShowTranscationList
 urlpatterns = [
     # path('home/',home,name="home"),
     # path('myhome/',Home.as_view(),name="myhome")
@@ -8,5 +8,5 @@ urlpatterns = [
     path("register",RegisterView.as_view(),name="register"),
     path('',Dashboard.as_view(),name='dashboard'),
     path('transcation',TranscationView.as_view(),name='transcation'),
-    path('transcation_list',name='transcation_list')
+    path('transcation_list',ShowTranscationList.as_view(),name='transcation_list')
 ]
