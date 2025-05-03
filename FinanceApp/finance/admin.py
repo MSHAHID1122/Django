e2dc4596d65a7d77b17042a18fe0a16cfd5cb781
MSHAHID1122 <samd13894@gmail.com>
2025-05-03 15:25:06 +0500

    adding transcation form

--- FinanceApp/finance/admin.py
+++ FinanceApp/finance/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import TranscationModel
+from .models import TransactionModel
 # Register your models here.
-admin.site.register(TranscationModel)
+admin.site.register(TransactionModel)
--- FinanceApp/finance/forms.py
+++ FinanceApp/finance/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from django import forms
-from .models import TranscationModel
+from .models import TransactionModel
 
 
 class RegistrationForm(UserCreationForm):
@@ -13,5 +13,5 @@ class RegistrationForm(UserCreationForm):
 
 class TranscationForm(forms.ModelForm):
     class Meta:
-        model = TranscationModel
+        model = TransactionModel
         fields = ['title','amount','Transcation_type','date','category']
